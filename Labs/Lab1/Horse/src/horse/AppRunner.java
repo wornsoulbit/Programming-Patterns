71e7cdb1146912905efaa9b9f3b63203eb1bde53
Alex Vasil <alexandervasil840@gmail.com>
2020-08-29 21:43:52 -0400

    Changed Object[] into the proper array

--- Labs/Lab1/Horse/src/horse/AppRunner.java
+++ Labs/Lab1/Horse/src/horse/AppRunner.java
@@ -9,14 +9,14 @@ import java.util.ArrayList;
 public class AppRunner {
     
     public static void main(String[] args) {
-        Object[] horseObjects = new Object[5];
+        Horse[] horseObjects = new Horse[5];
         horseObjects[0] = new Horse("Diamond", "White", 0);
         horseObjects[1] = new Horse("Jade", "Black", 0);
         horseObjects[2] = new RaceHorse("Crystal", "White", 0, 10);
         horseObjects[3] = new RaceHorse("Jake", "Brown", 0, 4);
         horseObjects[4] = new RaceHorse("James", "Light Brown", 0, 2);
         
-        ArrayList<Object> horseArrayList = new ArrayList<>();
+        ArrayList<Horse> horseArrayList = new ArrayList<>();
         horseArrayList.add(new Horse("Diamond", "White", 0));
         horseArrayList.add(new Horse("Jade", "Black", 0));
         horseArrayList.add(new RaceHorse("Crystal", "White", 0, 10));
