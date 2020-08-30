c0660cc65936254162816c827067d3f612b33874
Alex Vasil <alexandervasil840@gmail.com>
2020-08-29 21:43:35 -0400

    Changed Object[] into the proper array

--- Labs/Lab1/HotelRoom/src/hotelroom/UseHotelRoom.java
+++ Labs/Lab1/HotelRoom/src/hotelroom/UseHotelRoom.java
@@ -1,44 +1,49 @@
-
 package hotelroom;
 
 import java.util.ArrayList;
 
 /**
  * Test file.
+ *
  * @author Alex
  */
 public class UseHotelRoom {
+
     public static void main(String[] args) {
-        
+
         System.out.println("Inital Room/Rent rate test");
         HotelRoom room = new HotelRoom(32);
         System.out.println(room.toString());
         Suite nSuite = new Suite(0);
         System.out.println(nSuite.toString());
-        
+
         System.out.println("");
         System.out.println("Printing an array of HotelRooms");
-        
+
         //Array of 5 objects, 3 HotelRoom and 2 Suite objects.
-        Object[] rooms = new Object[5];
+        HotelRoom[] rooms = new HotelRoom[5];
         rooms[0] = new HotelRoom(1);
         rooms[1] = new HotelRoom(2);
         rooms[2] = new Suite(100);
         rooms[3] = new Suite(200);
         rooms[4] = new Suite(300);
-        
-        for (Object roomNum : rooms) { System.out.println(roomNum.toString()); }
-                
+
+        for (Object roomNum : rooms) {
+            System.out.println(roomNum.toString());
+        }
+
         System.out.println("");
-        
+
         //ArrayList of 4 objects, 2 HotelRoom and 2 Suite objects.
-        ArrayList<Object> rooms2 = new ArrayList<>();
+        ArrayList<HotelRoom> rooms2 = new ArrayList<>();
         rooms2.add(new HotelRoom(299));
         rooms2.add(new HotelRoom(100));
         rooms2.add(new Suite(50));
         rooms2.add(new Suite(45));
-        
+
         //Prints out the ArrayList using Lamda expression.
-        rooms2.forEach((roomNum) -> { System.out.println(roomNum.toString()); });
+        rooms2.forEach((roomNum) -> {
+            System.out.println(roomNum.toString());
+        });
     }
 }
