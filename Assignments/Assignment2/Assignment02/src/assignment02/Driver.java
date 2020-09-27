d65f6d2446d73fb33c1c1dd528d62d924b9b271c
Alex Vasil <alexandervasil840@gmail.com>
2020-09-27 17:03:19 -0400

    Assignment 2 code completed, just need to do jUnit test component

--- Assignments/Assignment2/Assignment02/src/assignment02/Driver.java
+++ Assignments/Assignment2/Assignment02/src/assignment02/Driver.java
@@ -5,44 +5,64 @@
  */
 package assignment02;
 
-
 /**
  *
  * @author Alex Vasil
  */
 public class Driver {
-    
+
     public static void main(String[] args) {
+        //<editor-fold defaultstate="collapsed" desc="Tests out removing, adding, union, intersection and difference methods.">
         MySet<Integer> intMySet = new MySet<>();
         //<editor-fold defaultstate="collapsed" desc="Adding numbers to intMySet">
         intMySet.add(2);
-        intMySet.add(2);
         intMySet.add(5);
-        intMySet.add(4);
-        intMySet.add(3);
-        intMySet.add(5);
-        intMySet.add(1);
-        intMySet.add(4);
-//</editor-fold>
+        intMySet.add(8);
+        intMySet.add(9);
+        //</editor-fold>
+
+        MySet<Integer> intMySet2 = new MySet<>();
+        //<editor-fold defaultstate="collapsed" desc="Adding numbers to intMySet2">
+        intMySet2.add(4);
+        intMySet2.add(5);
+        intMySet2.add(6);
+        intMySet2.add(7);
+        intMySet2.add(8);
+        //</editor-fold>
+
+        System.out.println("Union of set 1 and 2");
+        displaySet(intMySet.union(intMySet2)); //2, 4, 5, 6, 7, 8, 9
         
-        System.out.println("Inital Set");
+        System.out.println("\nIntersection of set 1 and 2");
+        displaySet(intMySet.intersection(intMySet2)); //5, 8
+
+        System.out.println("\nDifference of set 1 and 2");
+        displaySet(intMySet.difference(intMySet2)); //2, 9
+
+        System.out.println("\nInital Set");
         displaySet(intMySet);
-        
+        intMySet.remove(1);
         intMySet.remove(3);
         intMySet.remove(2);
-        
-        System.out.println("Set with removed number");
+
+        System.out.println("\nSet with removed number");
         displaySet(intMySet);
-        
+
         intMySet.add(30);
         intMySet.add(20);
-        System.out.println("Set with added numbers");
+        System.out.println("\nSet with added numbers");
         displaySet(intMySet);
         intMySet.remove(20);
-        System.out.println("Set with removed number");
+        System.out.println("\nSet with removed number");
         displaySet(intMySet);
+        //</editor-fold>
+
+        Integer intArr[] = {1, 2, 3, 4, 5};
+        MySet<Integer> intSet = new MySet<>(intArr);
+        System.out.println("\nInt set with a array.");
+        displaySet(intSet);
     }
-    
+
     public static void displaySet(MySet e) {
         for (int i = 0; i < e.size(); i++) {
             System.out.println(e.get(i));
--- Assignments/Assignment2/Assignment02/src/assignment02/MySet.java
+++ Assignments/Assignment2/Assignment02/src/assignment02/MySet.java
@@ -1,11 +1,12 @@
 package assignment02;
 
-import java.awt.List;
+import java.util.List;
 
 /**
- *
+ * A implementation of Set.
+ * 
  * @author Alex Vasil
- * @param <E>
+ * @param <E> Class type.
  */
 public class MySet<E> implements MyList<E> {
 
@@ -13,25 +14,44 @@ public class MySet<E> implements MyList<E> {
     private Node<E> tail;
     private int size;
 
+    /**
+     *
+     */
     public MySet() {
 
     }
 
+    /**
+     * Constructor of MySet with a given array.
+     * 
+     * @param array Array to be added to the set.
+     */
     public MySet(E[] array) {
         for (E element : array) {
             add(element);
         }
     }
 
-    public MySet(List otherList) {
-            
+    /**
+     * Constructor of MySet with a given list.
+     * 
+     * @param otherList List to be added to the set.
+     */
+    public MySet(List<E> otherList) {
+        otherList.forEach((element) -> { add(element); });
     }
 
+    /**
+     * Adds a element to a set.
+     * 
+     * @param element Element to be added to the set.
+     * @return If it was added.
+     */
     @Override
     public boolean add(E element) {
         if (head == null) {
             addElement(element);
-            
+
             if (tail == null) {
                 tail = head;
             }
@@ -47,7 +67,7 @@ public class MySet<E> implements MyList<E> {
                 }
                 current = current.next;
             }
-            
+
             if (!isDup) {
                 addElement(element);
                 return true;
@@ -56,7 +76,12 @@ public class MySet<E> implements MyList<E> {
 
         return false;
     }
-
+    
+    /**
+     * Adds a given element to the set.
+     * 
+     * @param element To be added.
+     */
     private void addElement(E element) {
         Node<E> newNode = new Node<>(element);
         newNode.next = head;
@@ -64,45 +89,140 @@ public class MySet<E> implements MyList<E> {
         size++;
     }
 
+    /**
+     * Removes a element from a set.
+     * 
+     * @param element Element to be removed.
+     * @return If it was removed.
+     */
     @Override
     public boolean remove(E element) {
         Node<E> current = head;
         Node<E> temp = null;
-        
+
         while (current != null && !current.element.equals(element)) {
             temp = current;
             current = current.next;
         }
-        
-        if (current == null) 
+
+        if (current == null) {
             return false;
-        if (temp == null) 
-            temp = current;
-        
-        temp.next = current.next;
-        size--;
-        return true;
+        } else {
+            if (current == head) {
+                head = head.next;
+            } else if (size == 1) {
+                head = tail = null;
+            } else {
+                temp.next = current.next;
+            }
+            size--;
+            return true;
+        }
     }
 
+    /**
+     * Combines two lists together. 
+     * 
+     * @param otherSet Set to be combined to the original set.
+     * @return The combined set.
+     */
     @Override
     public MySet<E> union(MySet<E> otherSet) {
-        return new MySet<>();
+        MySet<E> unionSet = new MySet<>();
+        Node<E> originalSetHead = head;
+        Node<E> otherSetHead = otherSet.head;
+
+        while (originalSetHead != null) {
+            unionSet.add(originalSetHead.element);
+            originalSetHead = originalSetHead.next;
+        }
+
+        while (otherSetHead != null) {
+            unionSet.add(otherSetHead.element);
+            otherSetHead = otherSetHead.next;
+        }
+        
+        return unionSet;
     }
 
+    /**
+     * Combines two lists elements that are the same in either list. 
+     * 
+     * @param otherSet Set to be combined to the original set.
+     * @return The combined set.
+     */
     @Override
     public MySet<E> intersection(MySet<E> otherSet) {
-        return new MySet<>();
+        MySet<E> interSet = new MySet<>();
+        Node<E> originalSetHead = head; 
+        Node<E> otherSetHead = otherSet.head; 
+
+        while (originalSetHead != null) {
+            while (otherSetHead != null) {
+                if (originalSetHead.element.equals(otherSetHead.element)) {
+                    interSet.add(otherSetHead.element);
+                    break;
+                }
+                otherSetHead = otherSetHead.next;
+            }
+            otherSetHead = otherSet.head;
+            originalSetHead = originalSetHead.next;
+        }
+
+        return interSet;
     }
 
+    /**
+     * Combines two lists elements that aren't the same in either list. 
+     * 
+     * @param otherSet Set to be combined to the original set.
+     * @return The combined set.
+     */
     @Override
     public MySet<E> difference(MySet<E> otherSet) {
-        return new MySet<>();
+        MySet<E> diffSet = new MySet<>();
+        Node<E> originalSetHead = head;
+        Node<E> otherSetHead = otherSet.head;
+        boolean isUnique = true;
+
+        while (originalSetHead != null) {
+            while (otherSetHead != null) {
+                if (originalSetHead.element.equals(otherSetHead.element)) {
+                    isUnique = false;
+                    break;
+                }
+                otherSetHead = otherSetHead.next;
+            }
+
+            if (isUnique) {
+                diffSet.add(originalSetHead.element);
+            }
+
+            isUnique = true;
+            otherSetHead = otherSet.head;
+            originalSetHead = originalSetHead.next;
+        }
+
+        return diffSet;
     }
 
+    /**
+     * The size of the set.
+     * 
+     * @return The size of the set.
+     */
+    @Override
     public int size() {
         return size;
     }
-
+    
+    /**
+     * Gets a element from the set.
+     * 
+     * @param index Element index to be obtained.
+     * @return Element at the given index.
+     */
+    @Override
     public E get(int index) {
         if (size != 0 && index < size && index >= 0) {
             int i = 0;
@@ -114,6 +234,90 @@ public class MySet<E> implements MyList<E> {
             return current.element;
         } else {
             throw new IndexOutOfBoundsException();
+        }
+    }
+
+    /**
+     * Clears the entire list.
+     */
+    @Override
+    public void clear() {
+        while (head != null) {
+            Node<E> temp = head;
+            head = head.next;
+            temp = null;
+        }
+    }
+
+    /**
+     * Checks if the list is empty.
+     * 
+     * @return If it's empty.
+     */
+    @Override
+    public boolean isEmpty() {
+        return size != 0;
+    }
+
+    /**
+     * Checks to see if element e is in the list.
+     * 
+     * @param e Element to be checked for in the list.
+     * @return If element e is in the list.
+     */
+    @Override
+    public boolean contains(E e) {
+        Node<E> current = head;
+        for (int i = 0; i < size; i++) {
+            if (current.element.equals(e)) {
+                return true;
+            } else {
+                current = current.next;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Gets the index of element e if it's in the list.
+     * 
+     * @param e Element to be checked if it's in the list.
+     * @return The index of the first found element if it's in the list, otherwise 
+     * it returns -1 if it's not in the list.
+     */
+    @Override
+    public int indexOf(E e) {
+        Node<E> current = head;
+        for (int i = 0; i < size; i++) {
+            if (current.equals(e)) {
+                return i;
+            } else {
+                current = current.next;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Sets the element at a given index.
+     * 
+     * @param index Element to be replaced.
+     * @param e Element that replaces the original element.
+     */
+    @Override
+    public void set(int index, E e) {
+        if (index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node<E> current = head;
+        for (int i = 0; i < size; i++) {
+            if (i == index) {
+                current.element = e;
+                break;
+            } else {
+                head = current.next;
+            }
         }
     }
 }
